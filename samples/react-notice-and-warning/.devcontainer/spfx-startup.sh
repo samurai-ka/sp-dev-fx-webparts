5f8f41f3898c707963ee7d71edfb88ed5f988f9d
Hagen Deike <hagen.deike@dm.de>
2026-05-18 12:58:37 +0000

    working dev container more or less...

--- samples/react-notice-and-warning/.devcontainer/spfx-startup.sh
+++ samples/react-notice-and-warning/.devcontainer/spfx-startup.sh
@@ -5,7 +5,7 @@ npm install
 ## commands to create dev certificate and copy it to the root folder of the project
 echo
 echo -e "\e[1;94mGenerating dev certificate\e[0m"
-gulp trust-dev-cert
+heft trust-dev-cert
 
 # Convert the generated PEM certificate to a  CER certificate
 openssl x509 -inform PEM -in ~/.rushstack/rushstack-serve.pem -outform DER -out ./spfx-dev-cert.cer
@@ -30,4 +30,4 @@ fi
 echo
 echo -e "\e[1;92mReady!\e[0m"
 
-echo -e "\n\e[1;94m**********\nOptional: if you plan on using gulp serve, don't forget to add the container certificate to your local machine. Please visit https://aka.ms/spfx-devcontainer for more information\n**********"
+echo -e "\n\e[1;94m**********\nOptional: if you plan on using heft start, don't forget to add the container certificate to your local machine. Please visit https://aka.ms/spfx-devcontainer for more information\n**********"
